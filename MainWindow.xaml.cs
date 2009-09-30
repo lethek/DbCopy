ed54fbdcb86574ab4163c0e745b60c676ebb177f
lethek <michael@lethek.net>
2009-09-30 19:55:02 +1000

    added localized tooltips.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data.SqlClient;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Windows;
